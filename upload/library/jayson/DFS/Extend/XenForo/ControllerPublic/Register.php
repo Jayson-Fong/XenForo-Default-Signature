349449b1e3460b6947cc0e0e33f3a7b7f4f6cd7e
Jayson Fong <32722526+Jayson-Fong@users.noreply.github.com>
2018-09-15 16:28:47 -0400

    Added Support For Custom User Fields

--- upload/library/jayson/DFS/Extend/XenForo/ControllerPublic/Register.php
+++ upload/library/jayson/DFS/Extend/XenForo/ControllerPublic/Register.php
@@ -3,13 +3,17 @@ class jayson_DFS_Extend_XenForo_ControllerPublic_Register extends XFCP_jayson_DF
     protected function _completeRegistration(array $user, array $extraParams = array()) {
         $signature = XenForo_Application::get('options')->jayson_dfs_signature;
         if (!empty($signature)) {
-                $vars = array(
+            $vars = array(
                 '{username}' => $user['username'],
                 '{user_id}' => $user['user_id'],
                 '{email}' => $user['email'],
                 '{gender}' => $user['gender'],
                 '{timezone}' => $user['timezone']
             );
+            $customFields = XenForo_Model::create('XenForo_Model_UserField')->getUserFieldValues($user['user_id']);
+            foreach ($customFields as $customField => $value) {
+                $vars["{customField.{$customField}}"] = $value; 
+            }
             $signature = strtr($signature, $vars);
             $writer = XenForo_DataWriter::create('XenForo_DataWriter_User');
             $writer->setExistingData($user);
